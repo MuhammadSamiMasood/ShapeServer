913fc2f6ec67f5207b0681a4d451f00267a4b74b
MuhammadSamiMasood <samimasood88@gmail.com>
2019-05-09 05:53:55 -0700

    Added relative path for json file and changed some code to read json file

--- src/main/java/com/ebricks/shape/servlet/Shapes.java
+++ src/main/java/com/ebricks/shape/servlet/Shapes.java
@@ -1,19 +1,18 @@
 package com.ebricks.shape.servlet;
 
+import org.apache.commons.io.IOUtils;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 
 
 public class Shapes extends HttpServlet {
 
-    String line;
+    //String line;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -21,13 +20,16 @@ public class Shapes extends HttpServlet {
         resp.setContentType("application/json");
         PrintWriter printWriter = resp.getWriter();
 
-        FileReader fileReader = new FileReader("C:\\Users\\Mesmer\\IdeaProjects\\shapeserver\\resources\\shapes.json");
+        FileReader fileReader = new FileReader(getServletContext().getRealPath("shapes.json"));
         BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-        String fileData = "";
-        while ((line = bufferedReader.readLine()) != null) {
-            fileData += line;
-        }
+        String fileData = IOUtils.toString(fileReader);
+
+
+        //String fileData = "";
+        //while ((line = bufferedReader.readLine()) != null) {
+        //    fileData += line;
+        //}
 
         bufferedReader.close();
 
